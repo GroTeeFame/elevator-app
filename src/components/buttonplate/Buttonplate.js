a53f9d9504614ab2f4828aacc14109c6e90f3d58
Rostyslav <rostik24051995@gmail.com>
2023-02-07 11:10:21 +0200

    del comm

--- src/components/buttonplate/Buttonplate.js
+++ src/components/buttonplate/Buttonplate.js
@@ -11,11 +11,6 @@ export const Buttonplate = ({addOperation, currentFloor, executionListUp, execut
                 <button key={3} className={`button ${currentFloor == 3 ? "button-current-floor" : executionListUp.includes(3) ? "button-up" : executionListDown.includes(3) ? "button-down" : ''}`} onClick={() => addOperation(3)} >3</button>
                 <button key={2} className={`button ${currentFloor == 2 ? "button-current-floor" : executionListUp.includes(2) ? "button-up" : executionListDown.includes(2) ? "button-down" : ''}`} onClick={() => addOperation(2)} >2</button>
                 <button key={1} className={`button ${currentFloor == 1 ? "button-current-floor" : executionListUp.includes(1) ? "button-up" : executionListDown.includes(1) ? "button-down" : ''}`} onClick={() => addOperation(1)} >1</button>
-                {/* <button key={5} className="button" onClick={() => addOperation(5)} >5</button>
-                <button key={4} className="button" onClick={() => addOperation(4)} >4</button>
-                <button key={3} className="button" onClick={() => addOperation(3)} >3</button>
-                <button key={2} className="button" onClick={() => addOperation(2)} >2</button>
-                <button key={1} className="button" onClick={() => addOperation(1)} >1</button> */}
             </div>
         </div>
     )
